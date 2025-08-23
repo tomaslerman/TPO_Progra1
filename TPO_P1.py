c35c0763b8fc38ed13786ca8e81db6ac6a063d2a
Lourdes Sarmiento <202308631+lourdes-sarmiento@users.noreply.github.com>
2025-08-22 22:53:34 -0300

    Se agrego funcion login

--- TPO_P1.py
+++ TPO_P1.py
@@ -1,11 +1,87 @@
     
+def login(matriz_login):
+    usuario = input("Ingrese su usuario: ")
+    contrasena = input("Ingrese su contraseña: ")
+    while not usuario.isalpha():
+        print("El usuario debe contener solo letras.")
+        usuario = input("Ingrese su usuario nuevamente: ")
+    while not contrasena.isalnum():
+        print("La contraseña debe contener solo caracteres alfanuméricos.")
+        contrasena = input("Ingrese su contraseña nuevamente: ")   
+    lista_contra=[dato for dato in matriz_login if dato[0] == usuario and dato[1] == contrasena]
+    while len(lista_contra)==0:
+        print("Usuario o contraseña incorrectos. Intente nuevamente.")
+        usuario = input("Ingrese su usuario: ")
+        contrasena = input("Ingrese su contraseña: ")
+        lista_contra=[dato for dato in matriz_login if dato[0] == usuario and dato[1] == contrasena]
+    print("Login ingresado correctamente para el usuario:", usuario)
+    
+    opcion=input("Si desea agregar usuario, Ingrese 1 , si desea eliminar usuario, Ingrese 2, si desea modificar usuario, Ingrese 3, si desea salir Ingrese -1") 
+    while opcion != "-1":
+        opcion = validar_opcion(opcion, 1, 3, ["1. Agregar usuario", "2. Eliminar usuario", "3. Modificar usuario", "-1. Salir"])
+        
+        if opcion == "1":
+            agregar_usuario(matriz_login)
+        elif opcion == "2":
+            eliminar_usuario(matriz_login)
+        elif opcion == "3":
+            modificar_usuario(matriz_login)
+   
+def agregar_usuario(matriz_login):
+    usuario = input("Ingrese nuevo usuario: ")
+    contrasena= input("Ingrese nueva contraseña: ")
+    while not usuario.isalpha():
+        print("El usuario debe contener solo letras.")
+        usuario = input("Ingrese nuevamente el usuario: ")
+    while not contrasena.isalnum():
+        print("La contraseña debe contener solo caracteres alfanuméricos.")
+        contrasena= input("Ingrese nuevamente contraseña: ") 
+    matriz_login.append([usuario, contrasena])
+    print("Usuario agregado correctamente.")      
+        
+   
+
+def eliminar_usuario(matriz_login):
+    usuario = input("Ingrese el usuario a eliminar: ")
+    pos = buscar_id(matriz_login, usuario)
+    while pos == -1:
+        print("El usuario no existe.")
+        usuario = input("Vuelva a ingresar el usuario a eliminar: ")
+        pos = buscar_id(matriz_login, usuario)
+    matriz_login.pop(pos)
+    print("Usuario eliminado correctamente.")    
+
+def modificar_usuario(matriz_login):
+    usuario = input("Ingrese el usuario a modificar: ")
+    fila=[dato for dato in matriz_login if dato[0] == usuario]
+    while fila==[]:
+        print("El usuario no existe.")
+        usuario = input("Vuelva a ingresar el usuario a modificar: ")
+        fila=[dato for dato in matriz_login if dato[0] == usuario]
+    pos=matriz_login.index(fila[0])
+    nuevo_usuario = input("Ingrese el nuevo nombre de usuario: ") 
+    while not nuevo_usuario.isalpha():
+        print("El usuario debe contener solo letras.")
+        nuevo_usuario = input("Ingrese nuevamente el nuevo nombre de usuario: ") 
+    while not contrasena.isalnum():
+        print("La contraseña debe contener solo caracteres alfanuméricos.")
+        contrasena= input("Ingrese nuevamente contraseña: ")
+    nueva_contrasena = input("Ingrese la nueva contraseña: ")
+    matriz_login[pos][0] = nuevo_usuario
+    matriz_login[pos][1] = nueva_contrasena
+    print("Usuario modificado correctamente")
+    
 def validar_opcion(opcion,inicio,fin,encabezado):   
     while opcion !=-1 and opcion < inicio or opcion > fin:
         print("Debe ingresar rangos de opcion del ",inicio,"al",fin)
-        menu(encabezado)
-        opcion=int(input("Ingrese nuevamente una opcion del Menu ventas: "))
+        mostrar_encabezado(encabezado)
+        opcion=int(input("Ingrese nuevamente una opcion: "))
     return opcion
 
+def mostrar_encabezado(encabezado):
+    print("Opciones disponibles:")
+    for i in range(len(encabezado)):
+        print(encabezado[i])
 
 def mostrar_matriz(titulos,matriz):
     filas=len(matriz)
@@ -17,35 +93,24 @@ def mostrar_matriz(titulos,matriz):
         for columna in range (columnas):
             print(matriz[fila][columna],end="\t")
         print()
-    
-            
-        
-def menu(lista_encabezados):
-    for encabezado in lista_encabezados:
-        print(encabezado, end="\t")
-    
-    opcion=int(input("Ingrese opcion deseada: "))
-    opcion = validar_opcion(opcion, 1, len(lista_encabezados), lista_encabezados)
-    return opcion
 
-
-def agregar_cliente(matriz_clientes):
+def agregar_cliente(matriz):
     cliente = []
     nombre = input("Ingrese el nombre: ")
     edad = int(input("Ingrese edad: "))
     obra_social = ingresar_id_obra_social(matriz_obras_sociales,encabezados_obras_sociales)#funcion para pedir obra social
     telefono = int(input("Ingrese un número de teléfono:"))
-    id_cliente = len(matriz_clientes) + 1  
+    id_cliente = len(matriz) + 1  
     cliente.append(id_cliente, obra_social, nombre, edad, telefono)
-    matriz_clientes.append(cliente)
+    matriz.append(cliente)
 
 def modificar_cliente(matriz_clientes, matriz_obras_sociales, encabezados_obras_sociales):
     id_cliente = int(input("Ingrese el ID del cliente a modificar: "))
-    pos = buscar_dato(matriz_clientes, id_cliente)
+    pos = buscar_id(matriz_clientes, id_cliente)
     while pos == -1:
         print("El ID del cliente es inválido")
         id_cliente = int(input("Vuelva a ingresar el ID del cliente: "))
-        pos = buscar_dato(matriz_clientes, id_cliente)
+        pos = buscar_id(matriz_clientes, id_cliente)
     nombre = input("Ingrese el nuevo nombre del cliente: ")
     edad = int(input("Ingrese la nueva edad del cliente: "))
     obra_social = ingresar_id_obra_social(matriz_obras_sociales,encabezados_obras_sociales)  # función para ingresar ID de obra social
@@ -56,21 +121,25 @@ def modificar_cliente(matriz_clientes, matriz_obras_sociales, encabezados_obras_
     matriz_clientes[pos][4] = telefono
     print("Cliente modificado correctamente.")
 
+def mostrar_matriz_cuadro(encabezados, matriz):
+    for i in range(len(encabezados)):
+        print(f"{encabezados[i]:<20}", end="")
+    print()
+    for fila in matriz:
+        for elemento in fila:
+            print(f"{elemento:<20}", end="")
+        print()
+
 def ingresar_id_obra_social(matriz_obras_sociales,encabezados_obras_sociales):
     print("Obras sociales disponibles:")
-    mostrar_matriz(encabezados_obras_sociales, matriz_obras_sociales)
+    mostrar_matriz_cuadro(encabezados_obras_sociales, matriz_obras_sociales)
     id_obra_social = int(input("Ingrese el ID de la obra social: "))
-    pos = buscar_dato(matriz_obras_sociales, id_obra_social)
+    pos = buscar_id(matriz_obras_sociales, id_obra_social)
     while pos == -1:
         print("El ID de la obra social es inválido")
         id_obra_social = int(input("Vuelva a ingresar el ID de la obra social: "))
-        pos = buscar_dato(matriz_obras_sociales, id_obra_social)
+        pos = buscar_id(matriz_obras_sociales, id_obra_social)
     return id_obra_social
-
-
-
-
-
 
 def agregar_producto(matriz_productos):
     producto = []
@@ -85,11 +154,11 @@ def agregar_producto(matriz_productos):
 
 def modificar_producto(matriz_productos):
     id=int(input("Ingrese el código del producto a modificar: "))
-    pos = buscar_dato(matriz_productos,id)
+    pos = buscar_id(matriz_productos,id)
     while pos==-1:
         print(" El código del producto es inválido")
         id = int(input("Vuelva a ingresar el código del producto: "))
-        pos = buscar_dato(matriz_productos,id)
+        pos = buscar_id(matriz_productos,id)
     descripcion = input("Ingrese la nueva descripción del producto: ")
     cant_stock = int(input("Ingrese la nueva cantidad en stock: "))
     cant_stock=validar_mayor_que(cant_stock,0)#funcion para validar mayores que que un numero(0)
@@ -99,6 +168,7 @@ def modificar_producto(matriz_productos):
     matriz_productos[pos][2] = cant_stock
     matriz_productos[pos][3] = precio_unit
     print("Producto modificado correctamente.")
+
 def validar_mayor_que(valor, minimo):
     while valor <= minimo:
         print(f"El valor debe ser mayor que {minimo}")
@@ -107,11 +177,11 @@ def validar_mayor_que(valor, minimo):
 
 def dar_baja_producto(matriz_productos):
     id_producto=int(input("Ingrese el ID del producto a dar de baja: "))
-    pos=buscar_dato(matriz_productos,id_producto)
+    pos=buscar_id(matriz_productos,id_producto)
     while pos==-1:
         print("Error el ID ingresado no es valido")
         id_producto = int(input("Vuelva a ingresar el ID del producto: "))
-        pos = buscar_dato(matriz_productos,id_producto)
+        pos = buscar_id(matriz_productos,id_producto)
     for i in range(len(matriz_productos[0])):
         print(matriz_productos[pos][i], end="\t")
     print()
@@ -155,7 +225,7 @@ def agregar_receta(matriz_recetas):
     receta = []
     codigo = len(matriz_recetas) + 1
     producto = int(input("Ingrese el código del producto: "))#funcion para buscar el producto y validar input mayor a (1)
-    encontrado = buscar_dato(matriz_productos,producto)
+    encontrado = buscar_id(matriz_productos,producto)
     while encontrado==-1:
         print("Error! Código de producto inválido")
         producto = int(input("Vuelva a ingresar el código del producto: "))
@@ -165,12 +235,11 @@ def agregar_receta(matriz_recetas):
     receta.append(codigo,producto,fecha,medico,cantidad)
     matriz_recetas.append(receta)
                    
-def buscar_dato(matriz,dato):
+def buscar_id(matriz,dato):
     for i in range(len(matriz)):
         if matriz[i][0] == dato:
             return i
-    return -1
-
+    return 
 
 def agregar_obra_social(matriz_obras_sociales):
     obra_social = []
@@ -185,11 +254,11 @@ def agregar_obra_social(matriz_obras_sociales):
 
 def modificar_obra_social(matriz_obras_sociales):
     id_obra_social = int(input("Ingrese el ID de la obra social a modificar: "))
-    pos = buscar_dato(matriz_obras_sociales, id_obra_social)
+    pos = buscar_id(matriz_obras_sociales, id_obra_social)
     while pos == -1:
         print("El ID de la obra social es inválido")
         id_obra_social = int(input("Vuelva a ingresar el ID de la obra social: "))
-        pos = buscar_dato(matriz_obras_sociales, id_obra_social)
+        pos = buscar_id(matriz_obras_sociales, id_obra_social)
     nombre = input("Ingrese el nuevo nombre de la obra social: ")
     descuento = int(input("Ingrese el nuevo descuento que aplica la obra social: "))
     while descuento < 0 or descuento > 100:
@@ -201,11 +270,11 @@ def modificar_obra_social(matriz_obras_sociales):
     
 def dar_baja_elementos(matriz):
     id_elemento = int(input("Ingrese el ID: "))
-    pos = buscar_dato(matriz,id_elemento)
+    pos = buscar_id(matriz,id_elemento)
     while pos==-1:
         print("Error! El ID ingresado es inválido")
         receta = int(input("Vuelva a ingresar el ID: "))
-        pos = buscar_dato(matriz,id_elemento)
+        pos = buscar_id(matriz,id_elemento)
     for i in range(len(matriz[0])):
         print(matriz[pos][i])
     confirmacion = int(input("Desea eliminar estos datos? (1 para SI o 2 para NO): "))
@@ -215,16 +284,138 @@ def dar_baja_elementos(matriz):
     else:
         print("Cancelando operación")
         enter = input("Volviendo a menu...")
-    
 
+def menu_principal():
+    opcion = 0
+    while opcion != -1:
+        print("Menu Principal")
+        mostrar_encabezado(encabezados_menu)
+        opcion = int(input("Seleccione una opción: "))
+        if opcion == 1:  # Ventas
+            submenu_ventas()
+        elif opcion == 2:  # Inventario
+            submenu_inventario()
+        elif opcion == 3:  # Clientes
+            submenu_clientes()
+        elif opcion == 4:  # Reportes
+            submenu_reportes()
+        elif opcion == -1:  # Terminar programa
+            print("Programa finalizado.")
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+def submenu_ventas():
+    opcion = 0
+    while opcion != -1:
+        print("Submenú Ventas")
+        mostrar_encabezado(encabezados_submenu_ventas)
+        opcion = int(input("Seleccione una opción: "))
+        opcion = validar_opcion(opcion, 1, 4, encabezados_submenu_ventas)
+        if opcion == 1:  # Agregar venta
+            agregar_venta_y_detalle(matriz_ventas)
+        elif opcion == 2:  # Modificar detalle de venta
+            modificar_venta(matriz_detalle_ventas)
+        elif opcion == 3:  # Dar baja venta
+            dar_baja_elementos(matriz_ventas)
+        elif opcion == 4:  # Mostrar lista completa
+            mostrar_matriz(matriz_ventas)
+        elif opcion == -1:  # Volver al menú principal
+            print("Volviendo al menú principal.")
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+def agregar_venta_y_detalle(matriz):
+    venta = []
+    id_venta = len(matriz) + 1
+    fecha = fechaYvalidacion()
+    id_cliente = int(input("Ingrese el ID del cliente: "))
+    pos_cliente = buscar_id(matriz_clientes, id_cliente)
+    while pos_cliente == -1:
+        print("Error! El ID del cliente es inválido")
+        id_cliente = int(input("Vuelva a ingresar el ID del cliente: "))
+        pos_cliente = buscar_id(matriz_clientes, id_cliente)
+    total = int(input("Ingrese el total de la venta: "))
+    venta.append(id_venta, fecha, id_cliente, total)
+    matriz.append(venta)
+
+def modificar_venta(matriz):
+    id_venta = int(input("Ingrese el ID de la venta a modificar: "))
+    pos = buscar_id(matriz, id_venta)
+    while pos == -1:
+        print("El ID de la venta es inválido")
+        id_venta = int(input("Vuelva a ingresar el ID de la venta: "))
+        pos = buscar_id(matriz, id_venta)
+    fecha = fechaYvalidacion()
+    total = int(input("Ingrese el nuevo total de la venta: "))
+    matriz[pos][1] = fecha
+    matriz[pos][3] = total
+    print("Venta modificada correctamente.")
+
+def submenu_inventario():
+    opcion = 0
+    while opcion != -1:
+        print("Submenú Inventario")
+        mostrar_encabezado(encabezados_submenu_inventario)
+        opcion = int(input("Seleccione una opción: "))
+        opcion = validar_opcion(opcion, 1, 4, encabezados_submenu_inventario)
+        if opcion == 1:  # Agregar producto
+            agregar_producto()
+        elif opcion == 2:  # Modificar Producto
+            modificar_producto()
+        elif opcion == 3:  # Dar baja producto
+            dar_baja_elementos(matriz_productos)
+        elif opcion == 4:  # Mostrar lista completa
+            mostrar_matriz(matriz_productos)
+        elif opcion == -1:  # Volver al menú principal
+            print("Volviendo al menú principal.")
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+def submenu_clientes():
+    opcion = 0
+    while opcion != -1:
+        print("Submenú Clientes")
+        mostrar_encabezado(encabezados_submenu_clientes)
+        opcion = int(input("Seleccione una opción: "))
+        opcion = validar_opcion(opcion, 1, 4, encabezados_submenu_clientes)
+        if opcion == 1:  # Agregar cliente
+            agregar_cliente(matriz_clientes)
+        elif opcion == 2:  # Modificar cliente
+            modificar_cliente(matriz_clientes)
+        elif opcion == 3:  # Dar baja cliente
+            dar_baja_elementos(matriz_clientes)
+        elif opcion == 4:  # Mostrar lista completa
+            mostrar_matriz(matriz_clientes)
+        elif opcion == -1:  # Volver al menú principal
+            print("Volviendo al menú principal.")
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+def submenu_reportes():
+    opcion = 0
+    while opcion != -1:
+        print("Submenú Reportes")
+        mostrar_encabezado(encabezados_sub_menu_reportes)
+        opcion = int(input("Seleccione una opción: "))
+        opcion = validar_opcion(opcion, 1, 4, encabezados_sub_menu_reportes)
+        if opcion == 1:  # Estadística de ventas
+            estadisticas_ventas()
+        elif opcion == -1:  # Volver al menú principal
+            print("Volviendo al menú principal.")
+        else:
+            print("Opción no válida. Intente nuevamente.")
 
 #programa principal
-encabezados_menu = ["1. Ventas","2. Inventario","3. Clientes","4. Reportes"]
-encabezados_submenu_ventas = ["1. Agregar venta","2. Modificar venta","3. Dar baja venta","4. Mostrar lista completa"]
-encabezados_submenu_inventario = ["1. Agregar producto","2. Modificar Producto","3. Dar baja producto","4. Mostrar lista completa"]
-encabezados_submenu_clientes = ["1. Agregar cliente","2. Modificar Cliente","3. Dar baja cliente","4. Mostrar lista completa"]
-encabezados_sub_menu_reportes = ["Estadística de ventas"]
+
+encabezados_menu = ["1. Ventas","2. Inventario","3. Clientes","4. Reportes","-1. Terminar programa"]
+encabezados_submenu_ventas = ["1. Agregar venta","2. Modificar venta","3. Dar baja venta","4. Mostrar lista completa", "-1. Volver a menu"]
+encabezados_submenu_inventario = ["1. Agregar producto","2. Modificar Producto","3. Dar baja producto","4. Mostrar lista completa", "-1. Volver a menu"]
+encabezados_submenu_clientes = ["1. Agregar cliente","2. Modificar Cliente","3. Dar baja cliente","4. Mostrar lista completa", "-1. Volver a menu"]
+encabezados_sub_menu_reportes = ["Estadística de ventas", "-1. Volver a menu"]
 encabezados_ventas = ["id_venta","fecha","id_cliente","total"]
+
+matriz_login=[["stephy","uade1010"],["tomas","uade2020"],["matias","uade3030"],["lourdes","uade4040"]]
+
 matriz_ventas = [[1, "2023-10-01", 1, 150],
                  [2, "2023-10-02", 2, 200],
                  [3, "2023-10-03", 1, 300],
@@ -268,11 +459,8 @@ matriz_obras_sociales = [
     [4, "Omint", 5],
     [5, "Osecac", 12]
 ]
-#menu(encabezados_menu)
-#prueba de funciones
-opcion = menu(encabezados_menu)
-resultado=modificar_producto(matriz_productos)
-mostrar_matriz(encabezados_productos, matriz_productos)
-print("resultado", resultado)
+
+menu_principal()  # Llamada al menú principal para iniciar el programa
+
 
     
