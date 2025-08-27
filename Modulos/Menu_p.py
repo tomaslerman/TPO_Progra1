c505343b7bb123e33b519e256e8f011d181d3f99
yumsteph <yumstephy@gmail.com>
2025-08-26 21:00:36 -0300

    Agregdo de módulo Menu y modificaciones de módulos Funciones_generales y Cliente

---
+++ Modulos/Menu_p.py
@@ -0,0 +1,23 @@
+import Ventas
+import Productos
+import Cliente
+import Funciones_generales
+
+def menu_principal():
+    opcion = 0
+    while opcion != -1:
+        print("Menu Principal")
+        Funciones_generales.mostrar_encabezado(encabezados_menu)
+        opcion = int(input("Seleccione una opción: "))
+        if opcion == 1:  # Ventas
+            Ventas.submenu_ventas()
+        elif opcion == 2:  # Inventario
+            Productos.submenu_inventario()
+        elif opcion == 3:  # Clientes
+            Cliente.submenu_clientes()
+        elif opcion == 4:  # Reportes
+            Funciones_generales.submenu_reportes()
+        elif opcion == -1:  # Terminar programa
+            print("Programa finalizado.")
+        else:
+            print("Opción no válida. Intente nuevamente.")
