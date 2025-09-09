50cdb7f4be818e7950923d647aa8e4ecd5ee51ce
yumsteph <yumstephy@gmail.com>
2025-09-09 12:55:46 -0300

    reset de codigo en programa principal

--- programa_principal.py
+++ programa_principal.py
@@ -1,9 +1,4 @@
-# Programa Principal
+import modulos
 
-# Este es un ejemplo de contenido para el archivo Programa_principal.py
-
-def main():
-    print("Hola, mundo!")
-
-if __name__ == "__main__":
-    main()
+modulos.login(modulos.matriz_login)
+modulos.menu_p()
