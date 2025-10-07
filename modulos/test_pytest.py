f3f1341ef78624b2dbb9359749ecf98473316c4d
yumsteph <yumstephy@gmail.com>
2025-10-07 15:07:55 -0300

    test de función stock por agotar

--- modulos/test_pytest.py
+++ modulos/test_pytest.py
@@ -1,3 +1,12 @@
-from 
-def test_func():
-    
+from .datos_de_prueba import *
+def stock_por_agotar(matriz_productos):
+    productos_agotarse=[fila for fila in matriz_productos if (fila[2])<=2]
+    return productos_agotarse # Retorna la lista de productos con stock menor al umbral. Con los datos actuales, debería retornar Paracetamol y Amoxicilina.
+
+def test_stock_por_agotar():
+    resultado = stock_por_agotar(matriz_productos)
+    esperado = [
+        [1, "Paracetamol", 1, 10],
+        [3, "Amoxicilina", 2, 20]
+    ]
+    assert resultado == esperado, f"Se esperaba {esperado} pero se obtuvo {resultado}"
